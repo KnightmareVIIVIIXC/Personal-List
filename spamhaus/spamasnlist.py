725fccf2d01fc41a9699cc09fb3a17ece2d60d3a
Knightmare <114373431+KnightmareVIIVIIXC@users.noreply.github.com>
2025-02-01 11:59:00 -0500

    Revert "Update spamasnlist.py"
    This reverts commit ec1601f954a493822e9c1010d86306914bac7001.

--- spamhaus/spamasnlist.py
+++ spamhaus/spamasnlist.py
@@ -1,20 +1,30 @@
+import os
 import requests
 import json
-import os
+
+
+def download_file(url, save_path):
+    response = requests.get(url)
+    with open(save_path, "wb") as file:
+        file.write(response.content)
+
 
 def is_valid_asn(asn):
     return asn.isdigit()
 
+
 def extract_asn_from_json(json_data):
     asns = set()
-    for entry in json_data:  # Directly iterate over the parsed JSON
+    for line in json_data:
         try:
+            entry = json.loads(line)
             asn = entry.get("asn")
             if asn and is_valid_asn(str(asn)):
                 asns.add(str(asn))
-        except (json.JSONDecodeError, TypeError): # Catch TypeError if entry is not a dict
+        except json.JSONDecodeError:
             pass
     return asns
+
 
 def save_asns_to_file(asns, output_file):
     sorted_asns = sorted(asns, key=lambda x: int(x.lstrip("AS")))
@@ -22,26 +32,26 @@ def save_asns_to_file(asns, output_file):
         for asn in sorted_asns:
             file.write(f"AS{asn}\n")
 
+
+def delete_file(file_path):
+    if os.path.exists(file_path):
+        os.remove(file_path)
+
+
 if __name__ == "__main__":
     file_url = "https://www.spamhaus.org/drop/asndrop.json"
     output_directory = ""
     output_file = "asnspam.txt"
+    json_file_path = "asndrop.json"
 
-    try:
-        response = requests.get(file_url)
-        response.raise_for_status()  # Raise an exception for bad status codes (4xx or 5xx)
+    download_file(file_url, json_file_path)
 
-        try:
-            json_data = response.json()  # Parse the JSON directly from the response
-            new_asns = extract_asn_from_json(json_data)
+    with open(json_file_path, "r") as json_file:
+        new_asns = extract_asn_from_json(json_file)
 
-            output_path = os.path.join(output_directory, output_file)
-            save_asns_to_file(new_asns, output_path)
+    output_path = os.path.join(output_directory, output_file)
+    save_asns_to_file(new_asns, output_path)
 
-            print(f"ASNs extracted, sorted, and saved to {output_path}")
+    delete_file(json_file_path)
 
-        except json.JSONDecodeError:
-            print("Error: Invalid JSON received from URL.")
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error downloading file: {e}")
+    print(f"ASNs extracted, sorted, and saved to {output_path}")
